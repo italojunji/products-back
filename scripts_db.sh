2bb2943b16c98005011e8b3616e2c4a01ede726b
italo.matsushita <italo_junji@yahoo.com.br>
2024-07-16 01:57:34 -0300

    updating scripts_db

--- scripts_db.sh
+++ scripts_db.sh
@@ -2,4 +2,4 @@
 # initdb.exe -U admin -A password -E utf8 -W -D C:\sw\proj\products-back\postgresql\db-data
 
 # Start database
-postgresql/pgsql/bin/pg_ctl -D C:/sw/proj/products-back/postgresql/db-data -l arquivolog start
+postgresql/pgsql/bin/pg_ctl -D ./postgresql/db-data -l arquivolog start
